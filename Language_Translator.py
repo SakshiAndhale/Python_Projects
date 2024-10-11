5e0a630861490a0813d2367b248f9d636effb0eb
Sakshi Andhale <103496510+SakshiAndhale@users.noreply.github.com>
2024-10-11 23:07:00 +0530

    Create Language_Translator.py

---
+++ Language_Translator.py
@@ -0,0 +1,23 @@
+from googletrans import Translator
+
+translator = Translator()
+
+language = {"bn": "Bangla",
+            "en": "English",
+            "ko": "Koren",
+            "fr": "French",
+            "de": "German",
+            "he": "Hebrew",
+            "hi": "Hindi",
+            "it": "Italian",
+            "ja": "Japanese",
+            'la': "Latin",
+            "ms": "Malay",
+            "ne": "Nepali",
+            "ru": "Russian",
+            "ar": "Arabic",
+            "zh": "Chinese",
+            "es": "Spanish"
+            }
+
+# TO Be Continued
